0c54af5ff2a5ad006e78c81efceb27e9c8fa74ce
lencraft <lennygrandjean8890@gmail.com>
2022-09-30 09:49:19 +0200

    Update script_environnement.js

--- src/script/script_environnement.js
+++ src/script/script_environnement.js
@@ -25,26 +25,3 @@ document.querySelectorAll('[class*="reveal-"]').forEach(function(r){
     observer.observe(r)
 })
 
-
-const ratio2 = .1
-const options2 = {
-    root: null,
-    rootMargin: '0px',
-    threshold: ratio
-}
-
-const handleIntersect2 = function(entries, observer2){
-    
-    entries.forEach(function(entry){
-        if (entry.intersectionRatio > ratio){
-            entry.target.classList.add('reveal-visible')
-            observer.unobserve(entry.target)
-        }
-    })
-}
-
-
-const observer2 = new IntersectionObserver(handleIntersect2, options)
-document.querySelectorAll('[class*="reveal-"]').forEach(function(r){
-    observer2.observe(r)
-})
