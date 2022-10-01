32651e64353d5803adf6626bba7cef90628b88e4
Clonestriker <102251062+Clonestriker@users.noreply.github.com>
2022-10-01 10:45:25 +0200

    slide

---
+++ src/script/scriptSlider.js
@@ -0,0 +1,10 @@
+if (document.getElementById("slider--text")) {
+
+
+    const slider = () => {
+        document.getElementById("slider--text").classList.add('fade-in');
+    };
+
+    slider();
+    
+}
