c38f67fb8ed392465b84e0736cf5836d59de27c8
Mathias <mathias.notter1@orange.fr>
2022-10-17 00:28:30 +0200

    update 9
    mise à niveau + début frise chronologique

--- src/script/script_environnement.js
+++ src/script/script_environnement.js
@@ -1,15 +1,3 @@
-// const navigation = document.querySelector('nav');
-
-// window.addEventListener('scroll', () => {
-
-// if(window.scrollY > 930){
-//     navigation.classList.add('anim-nav');
-// } else {
-//     navigation.classList.remove('anim-nav');
-// }
-
-// })
-
 const allRonds = document.querySelectorAll('.rond');
 const allBoxes = document.querySelectorAll('.box');
 
